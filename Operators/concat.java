f2dd63ca5b1d113f94e1ceea165cbc14037321f2
om <sumankunari645@gmail.com>
2025-07-11 14:42:48 +0530

    Updated with 6 operator precedence examples

---
+++ Operators/concat.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class concat {
+    
+  public static void main(String[] arg)
+  {
+    int a =10; int b=20;
+     String Age;
+     System.out.println("Age"+10);
+     System.out.println(a+b+"ok");
+     System.out.println("yes"+ a+b);
+   
+     byte x =5;
+     int y =10;
+     int p = x+y;
+     System.out.println(p); //byte promoted to int 
+
+     System.out.println("total"+(10+20));
+  }
+}
